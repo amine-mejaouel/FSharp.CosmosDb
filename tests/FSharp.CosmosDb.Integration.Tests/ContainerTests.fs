b90b872a0fa020cdcb37a4b313e5fc4ffd7adc72
Amine Mejaouel <45773049+amine-mejaouel@users.noreply.github.com>
2022-03-15 21:46:04 +0100

    add Integration.Tests

---
+++ tests/FSharp.CosmosDb.Integration.Tests/ContainerTests.fs
@@ -0,0 +1,11 @@
+module ContainerTests
+
+open Expecto
+
+[<Tests>]
+let tests =
+    testList
+        "Container"
+        [ test "containerCreate should create a container if it does not exists" {
+            failwith "not implemented"
+          } ]
---
+++ tests/FSharp.CosmosDb.Integration.Tests/Program.fs
@@ -0,0 +1,12 @@
+﻿module FSharp.CosmosDb.Analyzer.Tests
+
+open Expecto
+open Expecto.Logging
+
+let config =
+    { defaultConfig with
+          verbosity = Verbose
+          runInParallel = false }
+
+[<EntryPoint>]
+let main argv = runTestsInAssembly config argv
